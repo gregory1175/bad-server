1cb43de4dcaec4dd5548f82856838537c589a482
gregory <gredmyr@gmail.com>
2025-05-05 18:00:25 +0300

    fix cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,15 +15,12 @@ const app = express()
 
 app.use(cookieParser())
 
-app.use((_, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.ORIGIN_ALLOW || "http://localhost:5173");
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-    console.log('Response Headers:', res.getHeaders());  // Логирование заголовков
-    next();
-});
-
+app.use(cors({
+    origin: process.env.ORIGIN_ALLOW || 'http://localhost:5173',
+    credentials: true,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Authorization,Content-Type'
+}));
   
 app.use(express.static(path.join(__dirname, 'public')));
 
