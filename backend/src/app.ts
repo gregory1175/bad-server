1d100e520365a3d63c051415c2a7877689a306e0
gregory <gredmyr@gmail.com>
2025-05-05 14:11:40 +0300

    fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,7 @@ import routes from './routes'
 
 const { PORT = 3000 } = process.env
 const app = express()
-const originAllow = process.env.ORIGIN_ALLOW || 5173;
+const originAllow = process.env.ORIGIN_ALLOW || "5173";
 
 app.use(cookieParser())
 
