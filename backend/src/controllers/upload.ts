010d92ea41033e99f773a8b4956d31e7f5e5cc4d
gregory <gredmyr@gmail.com>
2025-05-05 17:11:40 +0300

    image size fix

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,6 +1,10 @@
-import { NextFunction, Request, Response } from 'express'
-import { constants } from 'http2'
-import BadRequestError from '../errors/bad-request-error'
+import { NextFunction, Request, Response } from 'express';
+import { constants } from 'http2';
+import BadRequestError from '../errors/bad-request-error';
+
+const MIN_SIZE = 2 * 1024; // 2 KB
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const MAX_SIZE = 10 * 1024 * 1024; // 10 MB
 
 export const uploadFile = async (
     req: Request,
@@ -8,19 +12,24 @@ export const uploadFile = async (
     next: NextFunction
 ) => {
     if (!req.file) {
-        return next(new BadRequestError('Файл не загружен'))
+        return next(new BadRequestError('Файл не загружен'));
     }
+    if (req.file.size < MIN_SIZE) {
+        return next(new BadRequestError('Файл слишком маленький (минимум 2 KB)'));
+    }
+
     try {
         const fileName = process.env.UPLOAD_PATH
             ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
-            : `/${req.file?.filename}`
+            : `/${req.file?.filename}`;
+
         return res.status(constants.HTTP_STATUS_CREATED).send({
             fileName,
             originalName: req.file?.originalname,
-        })
+        });
     } catch (error) {
-        return next(error)
+        return next(error);
     }
-}
+};
 
-export default {}
+export default {};
