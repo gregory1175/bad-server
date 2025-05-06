736fcd5ca357e935f9804db8991208c7e2e9230e
gregory <gredmyr@gmail.com>
2025-05-06 13:50:09 +0300

    fix?

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -11,16 +11,16 @@ export const uploadFile = async (
         return next(new BadRequestError('Файл не загружен'))
     }
     try {
-        const fileName = process.env.UPLOAD_PATH
-            ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
-            : `/${req.file?.filename}`
-        return res.status(constants.HTTP_STATUS_CREATED).send({
-            fileName,
-            originalName: req.file?.originalname,
-        })
-    } catch (error) {
-        return next(error)
-    }
+    const fileName = process.env.UPLOAD_PATH
+    ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
+    : `/${req.file?.filename}`
+return res.status(constants.HTTP_STATUS_CREATED).send({
+    fileName,
+    originalName: req.file?.originalname,
+})
+} catch (error) {
+return next(error)
+}
 }
 
 export default {}
