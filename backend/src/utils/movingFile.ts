37f670debd1e4bea2be19a258f4272f324f325cc
gregory <gredmyr@gmail.com>
2025-05-06 11:55:48 +0300

    delete doble encoding

--- backend/src/utils/movingFile.ts
+++ backend/src/utils/movingFile.ts
@@ -1,20 +1,16 @@
 import { existsSync, renameSync } from 'fs';
-import { extname, join } from 'path';
-import { randomUUID } from 'crypto';
+import { join } from 'path';
 
-function movingFile(imagePath: string, from: string, to: string): string {
-    const ext = extname(imagePath);
-    const newFileName = `${randomUUID()}${ext}`;
-    const imagePathTemp = join(from, imagePath);
-    const imagePathPermanent = join(to, newFileName);
+function movingFile(fileName: string, from: string, to: string): string {
+    const sourcePath = join(from, fileName);
+    const destPath = join(to, fileName);
 
-    if (!existsSync(imagePathTemp)) {
+    if (!existsSync(sourcePath)) {
         throw new Error('Ошибка при сохранении файла');
     }
 
-    renameSync(imagePathTemp, imagePathPermanent);
-
-    return newFileName;
+    renameSync(sourcePath, destPath);
+    return fileName;
 }
 
 export default movingFile;
